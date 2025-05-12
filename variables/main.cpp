07dfeb874c19eeb73b730e41eccd154ee63f0462
Richardlws <liu.bing66066@hotmail.com>
2025-05-12 13:03:34 +0800

    variables

--- variables/main.cpp
+++ variables/main.cpp
@@ -3,12 +3,30 @@
 int main()
 {
 
-    int x;
+    /*int x;
     x = 5;
     int y = 6;
+    int sum = x+y;
 
     std::cout << x << '\n';
     std::cout << y << '\n';
+    std::cout << sum << '\n';*/
+
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+    //double(number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    //single character
+    
+    std::cout<<price;
+
+
+    std::cout << days;
 
 
     return 0;
