a47e1b5bcdad21504d6a0d235380220dafa1587d
Richardlws <liu.bing66066@hotmail.com>
2025-05-12 16:38:04 +0800

    variables

--- variables/main.cpp
+++ variables/main.cpp
@@ -16,17 +16,31 @@ int main()
     int year = 2023;
     int days = 7;
 
-    //double(number including decimal)
+    // double(number including decimal)
     double price = 10.99;
     double gpa = 2.5;
     double temperature = 25.1;
 
-    //single character
-    
-    std::cout<<price;
+    // single character
+    char grade = 'A';
+    char initial = 'B';
 
+    //std::cout << initial;
+    char currency ='$';
 
-    std::cout << days;
+    //boolean(true or false)
+    bool student = false;
+    bool power = true;
+
+    bool forSale = true;
+    //String 
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food ="pizza";
+    std::string address="123 Fake St.";
+    std::cout<<"Hello "<<name<<'\n';
+    std::cout<<"You are "<<age<<" years old.";
+    //std::cout << name;
 
 
     return 0;
