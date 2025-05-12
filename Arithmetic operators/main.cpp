81100edb0192b11ac4c3c8c69cdcc2fed814cc16
Richardlws <liu.bing66066@hotmail.com>
2025-05-12 22:08:00 +0800

    Arithmetic operators

--- Arithmetic operators/main.cpp
+++ Arithmetic operators/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 int main()
 {
-    int students = 20;
-    //students = students + 1;
-    students+=1;
+    //int students = 20;
+    // students = students + 1;
+    // students += 1;
+    // students++;
+    // students = students - 1;
+    // students -= 1;
+    // students--;
+
+    //  students = students * 2;
+    //  students *= 2;
+    //  students = students / 2;
+    //  students /= 2;
+
+    // int remainder = students % 3;
+    // std::cout << remainder;
+
+    // std::cout << students;
+    int students = 6 - 5 + 4 * 3 / 2;
     std::cout << students;
 
     return 0;
