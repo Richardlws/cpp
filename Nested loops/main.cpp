5057129001ff19447f421efbeed5bf0cc24dd7a1
Richardlws <liu.bing66066@hotmail.com>
2025-05-14 13:11:56 +0800

    nestedloop

--- Nested loops/main.cpp
+++ Nested loops/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 int main(){
-    for(int i=1;i<=3;i++){
+    /*for(int i=1;i<=3;i++){
         for(int j = 1;j<=10;j++){
             
             std::cout<<j<<' ';
         }
         std::cout<<'\n';
-    }
+    }*/
+   int rows;
+   int columns;
+   char symbol;
 
+   std::cout<<"Please enter the symbol: ";
+   std::cin>>symbol;
+   std::cout<<"Please enter the rows: ";
+   std::cin>>rows;
+   std::cout<<"Please enter the columns: ";
+   std::cin>>columns;
+
+   for (int i=1;i<=rows;i++){
+        for(int j = 1;j<=columns;j++){
+            std::cout<<symbol;
+        }
+        std::cout<<'\n';
+   }
     return 0;
 }
