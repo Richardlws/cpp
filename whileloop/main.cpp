9b6728648e55aeeac355be9760e4d0d59fb56956
Richardlws <liu.bing66066@hotmail.com>
2025-05-14 12:47:59 +0800

    whileloop

--- whileloop/main.cpp
+++ whileloop/main.cpp
@@ -4,12 +4,16 @@ int main()
 
     std::string name;
 
-    while (name.empty())
+    /*while (name.empty())
     {
         std::cout << "Enter your name: ";
         std::getline(std::cin, name);
     }
-    std::cout << "Hello " << name;
+    std::cout << "Hello " << name;*/
+    while (1==1)
+    {
+        std::cout << "Help! I'm stuck in an infinite loop";
+    }
 
     return 0;
 }
