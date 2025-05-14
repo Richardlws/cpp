3abb59207e976340a91d3e7d9239e49a98060a0e
Richardlws <liu.bing66066@hotmail.com>
2025-05-14 12:54:54 +0800

    do while loop

--- Do while loops/main.cpp
+++ Do while loops/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-int main(){
-
-
+int main()
+{
+    int number;
+    /*std::cout << "Enter a positive #: ";
+    std::cin >> number;
+    while (number < 0)
+    {
+        std::cout << "Enter a positive #: ";
+        std::cin >> number;
+    }
+    std::cout << "The # is: " << number;*/
+    do
+    {
+        std::cout << "Enter a positive #:";
+        std::cin>>number;
+    } while (number < 0);
+    std::cout << "The # is: " << number;
 
     return 0;
 }
