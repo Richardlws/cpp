c71cc378a10303b0b393695d352153546c60b570
Richardlws <liu.bing66066@hotmail.com>
2025-05-15 20:59:31 +0800

    array

--- array/main.cpp
+++ array/main.cpp
@@ -2,13 +2,23 @@
 int main()
 {
 
-    std::string car[] = {"Corvette", "Mustang", "Camry"};
-    
-    car[0]="Camaro";
+    //std::string car[] = {"Corvette", "Mustang", "Camry"};
+
+    /*std::string cars[3];
+
+    cars[0]="Camaro";
+    cars[1]="Mustang";
+    cars[2]="Camry";
+    //car[0]="Camaro";
 
     for (int i = 0; i < 3; i++)
     {
-        std::cout << car[i] << "\n";
-    }
+        std::cout << cars[i] << "\n";
+    }*/
+    double prices[]={5.00,7.50,9.99,15.00};
+    std::cout<<prices[0]<<'\n';
+    std::cout<<prices[1]<<'\n';
+    std::cout<<prices[2]<<'\n';
+    std::cout<<prices[3]<<'\n';
     return 0;
 }
