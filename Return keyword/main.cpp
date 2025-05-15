a8721d6ee326a282f5e0036f7b1f5ca7407b4b2f
Richardlws <liu.bing66066@hotmail.com>
2025-05-15 08:54:09 +0800

    overloadfunction

--- Return keyword/main.cpp
+++ Return keyword/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 
-double square(double length);
+// double square(double length);
+// double cube(double length);
+std::string concatStrings(std::string string1, std::string string2);
 
 int main()
 {
+    std::string firstName = "Bro";
+    std::string lastName = "Code";
 
-    double length = 5.0;
-    std::cout << "The square's area is: " << square(length) << "cm^2";
+    std::cout << "Hello "<<concatStrings(firstName, lastName); 
+
+    /*double length = 6.0;
+    std::cout << "The square's area is: " << square(length) << "cm²\n";
+    std::cout << "The square's area is: " << cube(length) << "cm³\n";
 
     return 0;
 }
 double square(double length)
 {
     return length * length;
+}
+double cube(double length)
+{
+    return length * length* length;*/
+}
+
+std::string concatStrings(std::string string1, std::string string2)
+{
+    return string1 + " " + string2;
 }
