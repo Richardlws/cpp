dd2df1fd46b5985431bbb4387e20a1039d6bdb10
Richardlws <liu.bing66066@hotmail.com>
2025-05-17 10:33:16 +0800

    fillanarraywithuserinput

--- Fill an array with user input/main.cpp
+++ Fill an array with user input/main.cpp
@@ -3,17 +3,30 @@ int main()
 {
     std::string foods[5];
     int size = sizeof(foods) / sizeof(foods[0]);
+    std::string temp;
 
     for (int i = 0; i < size; i++)
     {
-        std::cout << "Enter a food you like #" << i+1 << ": ";
-        std::getline(std::cin, foods[i]);
+        std::cout << "Enter a food you like or 'q' to quit #" << i + 1 << ": ";
+        std::getline(std::cin, temp);
+        if (temp == "q")
+        {
+            break;
+        }
+        else
+        {
+            foods[i] = temp;
+        }
     }
     std::cout << "You like the following foods:\n";
 
-    for (std::string food : foods)
+    // for (std::string food : foods)
+    //{
+    //     std::cout << food << '\n';
+    // }
+    for (int i = 0; !foods[i].empty(); i++)
     {
-        std::cout << food << '\n';
+        std::cout << foods[i] << '\n';
     }
 
     return 0;
