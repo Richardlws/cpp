34567ff90f928dba73238e2bcc42209bc4c1efc6
Richardlws <liu.bing66066@hotmail.com>
2025-05-17 22:50:04 +0800

    recursion

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
-void walk(int steps);
+// void walk(int steps);
+int factorial(int num);
 int main()
 {
-
-    walk(10);
+    std::cout << factorial(6);
     return 0;
 }
-void walk(int steps)
+int factorial(int num)
+{
+    return (num <= 1) ? 1 : num * factorial(num - 1);
+}
+/*{
+    return 1;
+}else{
+    return num*factorial(num-1);
+}
+
+/*int result =1;
+for(int i=1;i<=num;i++){
+    result=result*i;
+}
+return result;*/
+
+
+/*void walk(int steps)
 {
     /*for(int i= 0;i<steps;i++){
 
         std::cout<<"You take a step!\n";
-    }*/
+    }
     if (steps > 0)
     {
         std::cout << "You take a step!\n";
         walk(steps - 1);
     }
-}
+}*/
