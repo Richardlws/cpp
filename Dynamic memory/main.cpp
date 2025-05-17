149f319c0838a9ece200d2647cbb27bfc2ee8263
Richardlws <liu.bing66066@hotmail.com>
2025-05-17 22:00:52 +0800

    dynamic memory

--- Dynamic memory/main.cpp
+++ Dynamic memory/main.cpp
@@ -2,11 +2,32 @@
 int main()
 {
 
-    int *pNum = NULL;
+    /*int *pNum = NULL;
     pNum = new int;
     *pNum = 21;
     std::cout << "address: " << pNum << '\n';
     std::cout << "value " << *pNum << '\n';
 
+
+    delete pNum;*/
+    char *pGrades = NULL;
+    int size;
+    std::cout << "How many grades to enter in?: ";
+    std::cin >> size;
+
+    pGrades = new char[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter grade #" << i + 1 << ":";
+        std::cin >> pGrades[i];
+    }
+    for (int i = 0; i < size; i++)
+    {
+
+        std::cout << pGrades[i] << " ";
+    }
+
+    delete[] pGrades;
     return 0;
 }
