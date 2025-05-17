9c0cca21633ddad19b13a1243145a77279854ce5
Richardlws <liu.bing66066@hotmail.com>
2025-05-17 11:17:15 +0800

    multiarray

--- multidimensionalarrays/main.cpp
+++ multidimensionalarrays/main.cpp
@@ -3,8 +3,17 @@ int main(){
     std::string cars[][3]={{"Mustang","Escape","F-150"},
                           {"Covette","Equinox","Silverado"},
                           {"Challenger","Durango","Ram 1500"}};
-    for(std::string car:cars[0]){
-        std::cout<<car<<"\n";
+    //for(std::string car:cars){
+    //    std::cout<<car<<" ";
+    //}
+    std::cout<<sizeof(std::string)<<"\n";
+    std::cout<<sizeof(cars)<<"\n";
+    std::cout<<sizeof(cars[0])<<"\n";
+    for(int i=0;i<sizeof(cars[0])/sizeof(cars[0][0]);i++){
+        for(int j=0;j<sizeof(cars)/sizeof(cars[0]);j++){
+            std::cout<<cars[i][j]<<" ";
+        }
+        std::cout<<"\n";
     }
 
     return 0;
