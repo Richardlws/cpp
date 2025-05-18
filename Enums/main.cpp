24c2260af80d6ee303e0b863653ad7eea898792a
Richardlws <liu.bing66066@hotmail.com>
2025-05-18 11:41:39 +0800

    enums

--- Enums/main.cpp
+++ Enums/main.cpp
@@ -11,6 +11,13 @@ enum Day
     saturday = 6
 };
 
+enum Flavor {vanilla,chocolate,strawberry,mint};
+enum Color{red,orange,yellow,green,blue,purple};
+enum Planet{mercury=4880,venus=12104,earth = 12756,
+            mars = 6794,jupiter=142984,saturn = 108728,
+            uranus = 51118,neptune= 49532,pluto=2320};
+
+
 int main(){
     
     
