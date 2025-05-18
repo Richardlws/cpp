1f2c96a78929fcdc8be645f93edefcd028d4134d
Richardlws <liu.bing66066@hotmail.com>
2025-05-18 10:58:45 +0800

    struct

--- Structs/main.cpp
+++ Structs/main.cpp
@@ -2,23 +2,23 @@
 struct student{
     std::string name;
     double gpa;
-    bool enrolled;
+    bool enrolled=true;
 };
 int main(){
     student student1;
     student1.name="Spongebob";
     student1.gpa=3.2;
-    student1.enrolled = true;
+    //student1.enrolled = true;
     
     student student2;
     student2.name="Patrick";
     student2.gpa=2.1;
-    student2.enrolled = true;
+    //student2.enrolled = true;
     
     student student3;
     student3.name="Squidward";
     student3.gpa=1.5;
-    student3.enrolled = false;
+    //student3.enrolled = false;
 
     std::cout<<student1.name<<'\n';
     std::cout<<student1.gpa<<'\n';
