3fccf8b63f50c211ba774c20b375c7495f1b01fa
Richardlws <liu.bing66066@hotmail.com>
2025-05-20 13:09:09 +0800

    Create main.cpp

---
+++ Constructors/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+class Student{
+    public:
+        std::string name;
+        int age;
+        double gpa;
+    Student(){
+        
+    }
+
+};
+int main(){
+    Student student1;
+    
+    
+    return 0;
+}
