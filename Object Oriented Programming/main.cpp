57f25c073036b48b16de21fd1d139a58d6fb6ecf
Richardlws <liu.bing66066@hotmail.com>
2025-05-20 12:59:14 +0800

    object oriented

--- Object Oriented Programming/main.cpp
+++ Object Oriented Programming/main.cpp
@@ -16,11 +16,38 @@ class Human{
         }
 
 };
+
+class Car{
+    public:
+    std::string make;
+    std::string model;
+    int year;
+    std::string color;
+
+    void accelerate(){
+        std::cout<<"You step on the gas!\n";
+    }
+    void brake(){
+        std::cout<<"You step on the brakes!\n";
+    }
+};
 int main(){
     Human human1;
     Human human2;
     Human human3;
+    Car car1;
+    car1.make="Ford";
+    car1.model="Mustang";
+    car1.year=2023;
+    car1.color="silver";
 
+    car1.accelerate();
+    car1.brake();
+
+    std::cout<<car1.make<<'\n';
+    std::cout<<car1.model<<'\n';
+    std::cout<<car1.year<<'\n';
+    std::cout<<car1.color<<'\n';
 
     human1.name="Rick";
     human1.occupation="scientist";
